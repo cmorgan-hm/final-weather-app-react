f10a7b471c7d1e3f4942361d762a6fba5326736f
cmorgan-hm <cmorgan.hm@gmail.com>
2023-06-03 14:20:39 +0330

    image link

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./WeatherForecast.css";
 
-
-export default function WeatherForecast () {
-    return (
-      <div className="ForecastContainer">
-        <div> Thu </div>
-        <img src="src/images/rain-day.gif" alt="description"></img>
-      </div>
-    );
+export default function WeatherForecast() {
+  return (
+    <div className="ForecastContainer">
+      <div> Thu </div>
+      <img src="../images/rain-day.gif" alt="description"></img>
+    </div>
+  );
 }
